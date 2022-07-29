3bd42e09dcc04afafd8e28079b2cfc75a6c4fdb8
BoubaM2i <boubacar.dembele@labom2iformation.fr>
2022-07-29 14:36:52 +0200

    fix: resolution du conflit de Profile Page

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-import ContactUsers from '../../components/contactUsers/ContactUsers'
-
-const ProfilePage = () => {
-  return (
-
-    <ContactUsers />
-  )
-}
-=======
-import { Navlist } from "../../components";
 
 const ProfilePage = () => {
     return (
         <div>
-            <Navlist />
+         
             <div>ProfilePage</div>
         </div>
     );
 };
->>>>>>> 234ce412a13735e7664937bb9badccbfcc70dbf5
 
 export default ProfilePage;
