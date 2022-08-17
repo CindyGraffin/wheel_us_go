39e0656664379455d7e6298eb1b098feacf1b51d
CindyGraffin <graffincindy@gmail.com>
2022-08-17 21:13:21 +0200

    feature/login: creation du custom hook useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+
+const useFetch = (url: string) => {
+    const [data, setData] = useState<any>([]);
+    const [loading, setLoading] = useState<any>(true);
+    const [error, setError] = useState<any>(null);
+
+    useEffect(() => {
+        const fetchData = async(): Promise<void> => {
+            try {
+                const response = await axios.get<any>(url);
+                setData(response.data);
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    setError('Axios error: ' + error.message);
+                } else {
+                    setError(error);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+
+    return [data, error, loading] as const;
+};
+
+export default useFetch;
