5a887828b06462c0232c63e1dd7a589401e1679d
CindyGraffin <graffincindy@gmail.com>
2022-08-19 00:02:57 +0200

    feature(login): mise a jour du state credentials du composant Login lors de changement des inputs

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,25 +1,62 @@
-import { useContext, useEffect } from "react";
+
+import { useContext, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { AuthContext } from "../../context/AuthContext";
+
 type LoginFormValues = {
     email: string;
     password: string;
 }
+interface ICredentials {
+	email: undefined | string;
+	password: undefined | string;
+}
 
 const LoginForm: React.FC<unknown> = () => {
-	const { register, handleSubmit, formState: {errors}} = useForm<LoginFormValues>();
+	const [credentials, setCredentials] = useState<ICredentials>({
+		email: undefined,
+		password: undefined
+	})
+	const {state, dispatch} = useContext(AuthContext);
+	const { register, handleSubmit, formState: {errors}, getValues} = useForm<LoginFormValues>({mode: "onChange"});
     const onSubmit: SubmitHandler<LoginFormValues> = data => console.log(data);
-    const {state, dispatch} = useContext(AuthContext)
+
 	useEffect(() => {
 		console.log(state.user);
-		
-	}, [])
-
+	}, [state.user])
+	useEffect(() => {
+		console.log(credentials);
+	}, [credentials])
+	useEffect(() => {
+		console.log(credentials);
+	}, [credentials])
+	const emailField = register('email', {required: true})
+	
+	const handleChange = () => {
+		setCredentials({
+			email: getValues('email'),
+			password: getValues('password')
+		})
+	}
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<input placeholder="Votre adresse mail" type="email" {...register("email", { required: true })} />
+			<input 
+				placeholder="Votre adresse mail" 
+				type="email" 
+				{...emailField}
+				onChange={(e) => {
+					emailField.onChange(e)
+					handleChange()
+				}}
+				
+				
+			/>
 			{errors.email && <span>Adresse mail requise</span>}
-			<input placeholder="Votre mot de passe" type="password" {...register("password", { required: true })} />
+			<input 
+				placeholder="Votre mot de passe" type="password" {...register("password", { required: true })} onChange={(e) => {
+					emailField.onChange(e)
+					handleChange()
+				}} />
 			{errors.password && <span>Mot de passe requis</span>}
 			<button type="submit">Se connecter</button> 
 		</form>
