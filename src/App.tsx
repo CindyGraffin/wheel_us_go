9b53496d35015114c004adebd1e43dc5cfba32de
CindyGraffin <graffincindy@gmail.com>
2022-08-23 21:38:06 +0200

    feature(refacto): onclick ajouté sur les boutons de création de salles

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function App() {
       <Routes>
         <Route path='/' element={<ConnectionPage/>}/>
         <Route path='/profile' element={<ProfilePage/>}/>
-        <Route path='/searchuser/' element={<SearchUser/>}/>
-        <Route path='/usertables/' element={<UserTables/>}/>
-        <Route path='/usermessages/' element={<UserMessages/>}/>
-        <Route path='/userfriends/' element={<UserFriends/>}/>
+        <Route path='/searchuser' element={<SearchUser/>}/>
+        <Route path='/usertables' element={<UserTables/>}/>
+        <Route path='/usermessages' element={<UserMessages/>}/>
+        <Route path='/userfriends' element={<UserFriends/>}/>
         <Route path='/premium' element={<PremiumPage/>}/>
         <Route path='/reglages' element={<ReglagesPage/>}/>
       </Routes>
