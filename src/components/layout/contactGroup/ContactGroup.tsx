29ab29d9efd011e7e3a8c5b6c119d5c78e15ad31
CindyGraffin <graffincindy@gmail.com>
2022-08-25 19:39:43 +0200

    feature(layout): css des messages en cas de non amis ou non groupes

--- src/components/layout/contactGroup/ContactGroup.tsx
+++ src/components/layout/contactGroup/ContactGroup.tsx
@@ -23,35 +23,46 @@ const ContactGroup: React.FC<ContactGroupProps> = ({
 				{icon}
 				<p>{title}</p>
 			</div>
-			<div className="contact-group">
-				{contacts.length > 0 ? (
-					contacts.map((contact: IUser) => (
+			
+				{contacts.length > 0 ? (<>
+                    <div className="contact-group">
+					{contacts.map((contact: IUser) => (
 						<Contact
 							key={contact.email}
 							firstName={contact.firstName}
 							lastName={contact.lastName}
 							imgSrc={contact.userImg}
 						/>
-					))
+					))}
+                    </div>
+                    <div className="contact-btn">
+                    <AiOutlinePlus className="contact-btn-icon" />
+                </div>
+                </>
 				) : classname === "contacts-group contacts" ? (
-					<div>
+                    <>
+					<div className="no-contacts-message">
 						<p>Vous n'avez pas encore de poulets à vos côtés ?</p>
 						<p>
 							Ajoutez un ami en cliquant sur le bouton ci dessous !
 						</p>
 					</div>
+                    <button className="add-contact">Ajouter un ami</button>
+            
+                    </>
 				) : (
-					<div>
-						<p>Vous n'avez pas encore de basse-cour ?</p>
+                    <>
+					<div className="no-contacts-message">
+						<p>Vous ne faites partie d'aucune basse-cour ?</p>
 						<p>
 							Créer un groupe en cliquant sur le bouton ci dessous !
 						</p>
 					</div>
+                    <button className="add-contact">Créer un groupe</button>
+                    </>
 				)}
-			</div>
-			<div className="contact-btn">
-				<AiOutlinePlus className="contact-btn-icon" />
-			</div>
+			
+			
 		</div>
 	);
 };
