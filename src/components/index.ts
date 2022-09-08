bbbb98eb82215336bf48fa13c9a2b4319306f08e
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-08 16:27:22 +0200

    chore(Loading): ajout d'un loader dashboard :art:

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
 import Badge from "./profile/badge/Badge";
+import DashboardLoading from "./UI/dashboard/LoadingScreen/LoadingScreen";
 import Layout from "./layout/Layout";
 import UserHeadband from "./userHeadband/userHeadband";
 
-export { 
-    Badge,
-    Layout,
-    UserHeadband
-};
+export { Badge, Layout, UserHeadband, DashboardLoading };
