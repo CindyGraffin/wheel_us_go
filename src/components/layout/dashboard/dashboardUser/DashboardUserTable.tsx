dc62a6e036962fd291b765761f4c6b8ddfb4647b
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-09 15:56:40 +0200

    refacto(dependances): retrait material ui

--- src/components/layout/dashboard/dashboardUser/DashboardUserTable.tsx
+++ src/components/layout/dashboard/dashboardUser/DashboardUserTable.tsx
@@ -1,119 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
 import IUser from "../../../../types/IUser";
-
-interface Column {
-    id: "firstname" | "lastname";
-    label: string;
-    minWidth?: number;
-    align?: "right";
-    format?: (value: number) => string;
-}
-
-const columns: readonly Column[] = [
-    { id: "firstname", label: "Prénom", minWidth: 170 },
-    { id: "lastname", label: "Nom", minWidth: 170 },
-];
+import "./dashboardUserTable.css";
 
 export interface DashboardUserTableProps {
     users: IUser[];
 }
 
 const DashboardUserTable: React.FC<DashboardUserTableProps> = ({ users }) => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
     return (
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <TableContainer>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{
-                                        minWidth: column.minWidth,
-                                    }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {users
-                            .slice(
-                                page * rowsPerPage,
-                                page * rowsPerPage + rowsPerPage
-                            )
-                            .sort((a, b) => {
-                                if (a.lastname < b.lastname) {
-                                    return -1;
-                                }
-                                if (a.lastname > b.lastname) {
-                                    return 1;
-                                }
-                                return 0;
-                            })
-                            .map((user, key) => {
-                                return (
-                                    <TableRow
-                                        hover
-                                        role="checkbox"
-                                        tabIndex={-1}
-                                        key={key}
-                                    >
-                                        {columns.map((column) => {
-                                            const value = user[column.id];
-                                            return (
-                                                <TableCell
-                                                    key={column.id}
-                                                    align={column.align}
-                                                >
-                                                    {column.format &&
-                                                    typeof value === "number"
-                                                        ? column.format(value)
-                                                        : value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                );
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={users.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
+        <div>
+            {users
+                .sort((a, b) => {
+                    if (a.lastname < b.lastname) {
+                        return -1;
+                    }
+                    if (a.lastname > b.lastname) {
+                        return 1;
+                    }
+                    return 0;
+                })
+                .map((user, key) => (
+                    <p>
+                        {user.firstname}&nbsp;{user.lastname}
+                    </p>
+                ))}
+        </div>
     );
 };
 
