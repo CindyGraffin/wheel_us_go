6ced90657ed1417cbf15b212e210aeb8409f7c25
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-09 16:57:21 +0200

    chore(dashboardUser): création du tableau & design :art:

--- src/components/layout/dashboard/dashboardUser/DashboardUserTable.tsx
+++ src/components/layout/dashboard/dashboardUser/DashboardUserTable.tsx
@@ -10,21 +10,35 @@ export interface DashboardUserTableProps {
 const DashboardUserTable: React.FC<DashboardUserTableProps> = ({ users }) => {
     return (
         <div>
-            {users
-                .sort((a, b) => {
-                    if (a.lastname < b.lastname) {
-                        return -1;
-                    }
-                    if (a.lastname > b.lastname) {
-                        return 1;
-                    }
-                    return 0;
-                })
-                .map((user, key) => (
-                    <p>
-                        {user.firstname}&nbsp;{user.lastname}
-                    </p>
-                ))}
+            <div className="grid_header">
+                <div className="grid_header_name grid_name">Nom</div>
+                <div className="grid_header_email grid_email">Email</div>
+                <div className="grid_header_role grid_role">Rôle</div>
+                <div className="grid_header_actions grid_actions">Actions</div>
+            </div>
+            <div className="grid_content">
+                {users
+                    .sort((a, b) => {
+                        if (a.lastname < b.lastname) {
+                            return -1;
+                        }
+                        if (a.lastname > b.lastname) {
+                            return 1;
+                        }
+                        return 0;
+                    })
+                    .map((user, key) => (
+                        <div key={key} className="grid_items">
+                            <div className="grid_img">img</div>
+                            <div className="grid_name">
+                                {user.firstname}&nbsp;{user.lastname}
+                            </div>
+                            <div className="grid_email">{user.email}</div>
+                            <div className="grid_role">{user.role}</div>
+                            <div className="grid_actions">TODO: ACTIONS</div>
+                        </div>
+                    ))}
+            </div>
         </div>
     );
 };
