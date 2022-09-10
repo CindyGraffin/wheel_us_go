c2bb229a63b8bbb1966599b02d9d9a4e6867da08
Roland <rolandleteneur@brik.com>
2022-09-10 22:37:18 +0200

    test:(dashboard): création test dashboard :white_check_mark:

---
+++ src/components/UI/dashboard/AdminProfile/AdminProfile.spec.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable testing-library/no-node-access */
+import "@testing-library/jest-dom";
+import { cleanup, render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import AdminProfile from "./AdminProfile";
+
+describe("Test sur le composant DashBoardLayout", () => {
+    let container: HTMLElement;
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.appendChild(container);
+    });
+
+    afterEach(cleanup);
+
+    test("doit fournir un rendu", () => {
+        const users = {
+            firstname: "TestName",
+            lastname: "TestLastName",
+            email: "test@mail.com",
+            role: "user",
+            password: "1234",
+            outingPart: 0,
+            outingCre: 0,
+            city: "lille",
+        };
+        render(
+            <BrowserRouter>
+                <AdminProfile user={users} />
+            </BrowserRouter>
+        );
+    });
+});
