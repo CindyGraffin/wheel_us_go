37c2dec301771a8eee96d211d364185887bc19f9
CindyGraffin <graffincindy@gmail.com>
2022-09-12 22:15:02 +0200

    refacto(layout): rightLayout composant changement style

--- src/components/layout/leftLayout/LeftLayout.tsx
+++ src/components/layout/leftLayout/LeftLayout.tsx
@@ -1,38 +1,38 @@
-import './leftLayout.css';
-import { IoRestaurantOutline, BiBeer, TbSofa } from '../../../icons/index';
-import { CreateRoom } from '../index';
+import "./leftLayout.css";
+import { IoRestaurantOutline, BiBeer, TbSofa } from "../../../icons/index";
+import { CreateRoom } from "../index";
 
 const LeftLayout: React.FC<unknown> = () => {
-    const roomsCreation = [
+	const roomsCreation = [
 		{
-			typeRoom: 'RESTAURANT',
-			icon: <IoRestaurantOutline className="create-room-icon" />
+			typeRoom: "RESTAURANT",
+			icon: <IoRestaurantOutline className="create-room-icon" />,
 		},
 		{
-			typeRoom: 'BAR',
-			icon: <BiBeer className="create-room-icon" />
+			typeRoom: "BAR",
+			icon: <BiBeer className="create-room-icon" />,
 		},
 		{
-			typeRoom: 'HOME',
-			icon: <TbSofa className="create-room-icon" />
-		}
+			typeRoom: "HOME",
+			icon: <TbSofa className="create-room-icon" />,
+		},
 	] as const;
-    return (
-        <div className="left__container">
-                <div className="logo__container">
-                    <img src={require("./logo.png")} alt="" />
-                </div>
-                <div className="create-rooms__container">
-                    {roomsCreation.map((room) => (
-                        <CreateRoom
-                            icon={room.icon}
-                            typeRoom={room.typeRoom}
-                            key={room.typeRoom}
-                        />
-                    ))}
-                </div>
-        </div>
-    );
+	return (
+		<div className="left__container">
+			<div className="logo__container">
+				<img src={require("./logo.png")} alt="" />
+			</div>
+			<div className="create-rooms__container">
+				{roomsCreation.map((room) => (
+					<CreateRoom
+						icon={room.icon}
+						typeRoom={room.typeRoom}
+						key={room.typeRoom}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default LeftLayout;
