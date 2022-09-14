87f353c290b2883502da44e08e9933ad553a29cd
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-14 09:56:36 +0200

    feat(auth): création du service de deconnexion :sparkles:

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,13 +1,18 @@
-import axios from "axios";  
-class UserService {   
-
+import { redirect } from "./../utils/redirect";
+import axios from "axios";
+class UserService {
     getUserById = (id: string) => {
-        return axios.get(`http://localhost:8800/api/users/${id}`)
-    }
+        return axios.get(`http://localhost:8800/api/users/${id}`);
+    };
 
-    getFriendsByUserId = (id: string) => {     
+    getFriendsByUserId = (id: string) => {
         return axios.get(`http://localhost:8800/api/users/friends/${id}`);
-    }; 
-}  
+    };
+
+    logOut = (): void => {
+        localStorage.clear();
+        redirect("/");
+    };
+}
 
 export const userService = Object.freeze(new UserService());
