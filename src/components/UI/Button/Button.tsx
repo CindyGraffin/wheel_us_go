c4d42d2eccfb3bfdf2daf442bc6297c03fee48c0
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-14 11:06:21 +0200

    chore(ui): ajout d'un boutton générique :art:

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from "react";
+import clsx from "clsx";
+import "./button.css";
+
+const COLORS = {
+    brown: { basic: "brown_button_basic" },
+} as const;
+
+type ButtonVariant = "basic";
+export type ButtonColor = keyof typeof COLORS;
+
+export interface ButtonProps {
+    children: ReactElement | string | number;
+    color?: ButtonColor;
+    type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
+    variant?: ButtonVariant;
+    onClick?: (e: React.MouseEvent) => void;
+    className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    type = "button",
+    onClick = () => {},
+    color = "brown",
+    variant = "basic",
+    children,
+}) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={clsx("button_className", COLORS[color][variant])}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
