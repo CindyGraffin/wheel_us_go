a0ef73d98f62fe6e68bb24d99026d7c8c932cc61
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-22 11:38:20 +0200

    chore(reports): ajout de la structure de la page des signalements

--- src/pages/dashboard-reportsList/DashboardRepportsList.tsx
+++ src/pages/dashboard-reportsList/DashboardRepportsList.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { FiAlertCircle } from "react-icons/fi";
+import DashboardLayout from "../../components/layout/dashboard/dashboard/DashboardLayout";
 import "./dashboardRepportsList.css";
 
 const DashboardRepportsList: React.FC<unknown> = () => {
     return (
-        <div>
-            <p>DashboardRepportsList</p>
-        </div>
+        <DashboardLayout>
+            <div>
+                <div className="dashboard_reports_title">
+                    <p>Signalements</p>
+                    <FiAlertCircle className="dashboard_reports_icon" />
+                </div>
+                <div>inserer table ici</div>
+            </div>
+        </DashboardLayout>
     );
 };
 
