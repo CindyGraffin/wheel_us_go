751587af64a94b11e7f4b24ce8b1b32b6841bb2c
EresseaNui <leteneur.roland@hotmail.fr>
2022-10-11 10:07:39 +0200

    feat(userProfile): ajout des profil utilisateur :sparkles:

--- src/pages/profile/Me/ProfilePage.tsx
+++ src/pages/profile/Me/ProfilePage.tsx
@@ -1,111 +1,132 @@
 import "./profilePage.css";
 
-import { AiOutlineStar, BiBeer, BiHomeAlt, BsPeople, FaGlassMartiniAlt, GiBabyFace, GiCartwheel, GiClover, GiMeal, GiRoastChicken, HiOutlineBadgeCheck, HiOutlinePhotograph, IoCalendarOutline, RiShirtLine, TbBrandAirtable } from "../../../icons/index";
+import {
+    AiOutlineStar,
+    BiBeer,
+    BiHomeAlt,
+    BsPeople,
+    FaGlassMartiniAlt,
+    GiBabyFace,
+    GiCartwheel,
+    GiClover,
+    GiMeal,
+    GiRoastChicken,
+    HiOutlineBadgeCheck,
+    HiOutlinePhotograph,
+    IoCalendarOutline,
+    RiShirtLine,
+    TbBrandAirtable,
+} from "../../../icons/index";
 
 import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { Badge, Layout, UserHeadband } from "../../../components";
 
-
 const ProfilePage: React.FC<unknown> = () => {
+    const badges = [
+        {
+            classname: "badge__container active",
+            badgeText: "Petit Poulet",
+            icon: <GiBabyFace className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Chicken",
+            icon: <GiRoastChicken className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Chanceux",
+            icon: <GiClover className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Dresscode adept'",
+            icon: <RiShirtLine className="badge active" />,
+        },
 
-	const badges = [
-		{
-			classname: 'badge__container active',
-			badgeText: 'Petit Poulet',
-			icon: <GiBabyFace className="badge active"/>
-		},
-		{
-			classname: 'badge__container active',
-			badgeText: 'Chicken',
-			icon: <GiRoastChicken className="badge active"/>
-		},
-		{
-			classname: 'badge__container active',
-			badgeText: 'Chanceux',
-			icon: <GiClover className="badge active"/>
-		},
-		{
-			classname: 'badge__container active',
-			badgeText: "Dresscode adept'",
-			icon: <RiShirtLine className="badge active"/>
-		},
-		
-		{
-			classname: 'badge__container active',
-			badgeText: 'At Home',
-			icon: <BiHomeAlt className="badge active"/>
-		},
-		{
-			classname: 'badge__container active',
-			badgeText: 'Adepte des apéros',
-			icon: <BiBeer className="badge active active"/>
-		},
-		{
-			classname: 'badge__container active',
-			badgeText: 'Souriez, photo',
-			icon: <HiOutlinePhotograph className="badge active"/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: 'En groupe',
-			icon: <BsPeople className="badge"/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: 'Créateur de tables',
-			icon: <TbBrandAirtable className="badge"/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: 'Dernière minute',
-			icon: <IoCalendarOutline className="badge"/>
-		},
-		
-		{
-			classname: 'badge__container',
-			badgeText: 'Accro aux bars',
-			icon: <FaGlassMartiniAlt className="badge"/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: 'Adepte des restos',
-			icon: <GiMeal className="badge "/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: "Fan de l'aléatoire",
-			icon: <GiCartwheel className="badge"/>
-		},
-		{
-			classname: 'badge__container',
-			badgeText: 'Membre premium',
-			icon: <AiOutlineStar className="badge"/>
-		},
-	]
+        {
+            classname: "badge__container active",
+            badgeText: "At Home",
+            icon: <BiHomeAlt className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Adepte des apéros",
+            icon: <BiBeer className="badge active active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Souriez, photo",
+            icon: <HiOutlinePhotograph className="badge active" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "En groupe",
+            icon: <BsPeople className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Créateur de tables",
+            icon: <TbBrandAirtable className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Dernière minute",
+            icon: <IoCalendarOutline className="badge" />,
+        },
 
-	const {state} = useContext(AuthContext);
+        {
+            classname: "badge__container",
+            badgeText: "Accro aux bars",
+            icon: <FaGlassMartiniAlt className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Adepte des restos",
+            icon: <GiMeal className="badge " />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Fan de l'aléatoire",
+            icon: <GiCartwheel className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Membre premium",
+            icon: <AiOutlineStar className="badge" />,
+        },
+    ];
 
-	return (
-		<Layout>
-			<div className="profile__container">
-				<div className="headband">
-					<UserHeadband user={state.user}/>
-					<div className="badges__container">
-						<div className="badges-title">
-							<HiOutlineBadgeCheck className="badge-icon" />
-							<p>MES BADGES</p>
-						</div>
-						<div className="badges">
-							{badges.map((badge, i) => {
-								return <Badge key={i} badgeText={badge.badgeText} icon={badge.icon} classname={badge.classname!}/>;
-							})}
-						</div>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
-}
+    const { state } = useContext(AuthContext);
+
+    return (
+        <Layout>
+            <div className="profile__container">
+                <div className="headband">
+                    <UserHeadband user={state.user} />
+                    <div className="badges__container">
+                        <div className="badges-title">
+                            <HiOutlineBadgeCheck className="badge-icon" />
+                            <p>MES BADGES</p>
+                        </div>
+                        <div className="badges">
+                            {badges.map((badge, i) => {
+                                return (
+                                    <Badge
+                                        key={i}
+                                        badgeText={badge.badgeText}
+                                        icon={badge.icon}
+                                        classname={badge.classname!}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
 export default ProfilePage;
--- src/pages/profile/UserProfile/UserProfile.tsx
+++ src/pages/profile/UserProfile/UserProfile.tsx
@@ -1,15 +1,135 @@
-import React from "react";
+import { Badge, Layout, UserHeadband } from "../../../components/index";
 import "./userprofile.css";
+import useFetch from "../../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import {
+    AiOutlineStar,
+    BiBeer,
+    BiHomeAlt,
+    BsPeople,
+    FaGlassMartiniAlt,
+    GiBabyFace,
+    GiCartwheel,
+    GiClover,
+    GiMeal,
+    GiRoastChicken,
+    HiOutlineBadgeCheck,
+    HiOutlinePhotograph,
+    IoCalendarOutline,
+    RiShirtLine,
+    TbBrandAirtable,
+} from "../../../icons/index";
 
 export interface UserProfileProps {
     className?: string;
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({ className = "" }) => {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const { userId } = useParams();
+    const { data: user } = useFetch(`/users/${userId}`);
+
+    const badges = [
+        {
+            classname: "badge__container active",
+            badgeText: "Petit Poulet",
+            icon: <GiBabyFace className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Chicken",
+            icon: <GiRoastChicken className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Chanceux",
+            icon: <GiClover className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Dresscode adept'",
+            icon: <RiShirtLine className="badge active" />,
+        },
+
+        {
+            classname: "badge__container active",
+            badgeText: "At Home",
+            icon: <BiHomeAlt className="badge active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Adepte des apéros",
+            icon: <BiBeer className="badge active active" />,
+        },
+        {
+            classname: "badge__container active",
+            badgeText: "Souriez, photo",
+            icon: <HiOutlinePhotograph className="badge active" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "En groupe",
+            icon: <BsPeople className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Créateur de tables",
+            icon: <TbBrandAirtable className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Dernière minute",
+            icon: <IoCalendarOutline className="badge" />,
+        },
+
+        {
+            classname: "badge__container",
+            badgeText: "Accro aux bars",
+            icon: <FaGlassMartiniAlt className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Adepte des restos",
+            icon: <GiMeal className="badge " />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Fan de l'aléatoire",
+            icon: <GiCartwheel className="badge" />,
+        },
+        {
+            classname: "badge__container",
+            badgeText: "Membre premium",
+            icon: <AiOutlineStar className="badge" />,
+        },
+    ];
+
     return (
-        <div>
-            <p>UserProfile</p>
-        </div>
+        <Layout>
+            <div className="profile__container">
+                <div className="headband">
+                    <UserHeadband user={user} />
+                    <div className="badges__container">
+                        <div className="badges-title">
+                            <HiOutlineBadgeCheck className="badge-icon" />
+                            <p>SES BADGES</p>
+                        </div>
+                        <div className="badges">
+                            {badges.map((badge, key) => {
+                                return (
+                                    <Badge
+                                        key={key}
+                                        badgeText={badge.badgeText}
+                                        icon={badge.icon}
+                                        classname={badge.classname!}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 };
 
