080c1e895268d348a261a2279685cd48f9d50b0e
EresseaNui <leteneur.roland@hotmail.fr>
2022-10-11 10:21:24 +0200

    fix(refactp): retrait service mal placer, code mort  :coffin:

--- src/pages/userFriends/UserService/userService.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-class UserService {
-  getFriendsByUserId = (id: string) => {
-    return axios.get(`http://localhost:8800/api/users/friends/${id}`);
-  };
-}
-
-export const userService = Object.freeze(new UserService());
