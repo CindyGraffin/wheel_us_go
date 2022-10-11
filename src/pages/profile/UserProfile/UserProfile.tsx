7def1233ff013bb5f22e406eb510f4e9e34679bf
EresseaNui <leteneur.roland@hotmail.fr>
2022-10-11 10:27:38 +0200

    fix(refacto): ajout d'un utils badgesList :recycle:

--- src/pages/profile/UserProfile/UserProfile.tsx
+++ src/pages/profile/UserProfile/UserProfile.tsx
@@ -3,101 +3,13 @@ import "./userprofile.css";
 import useFetch from "../../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import {
-    AiOutlineStar,
-    BiBeer,
-    BiHomeAlt,
-    BsPeople,
-    FaGlassMartiniAlt,
-    GiBabyFace,
-    GiCartwheel,
-    GiClover,
-    GiMeal,
-    GiRoastChicken,
     HiOutlineBadgeCheck,
-    HiOutlinePhotograph,
-    IoCalendarOutline,
-    RiShirtLine,
-    TbBrandAirtable,
 } from "../../../icons/index";
+import { badges } from "../../../utils/badgeList";
 
 const UserProfile: React.FC<unknown> = () => {
     const { userId } = useParams();
     const { data: user } = useFetch(`/users/${userId}`);
-
-    const badges = [
-        {
-            classname: "badge__container active",
-            badgeText: "Petit Poulet",
-            icon: <GiBabyFace className="badge active" />,
-        },
-        {
-            classname: "badge__container active",
-            badgeText: "Chicken",
-            icon: <GiRoastChicken className="badge active" />,
-        },
-        {
-            classname: "badge__container active",
-            badgeText: "Chanceux",
-            icon: <GiClover className="badge active" />,
-        },
-        {
-            classname: "badge__container active",
-            badgeText: "Dresscode adept'",
-            icon: <RiShirtLine className="badge active" />,
-        },
-
-        {
-            classname: "badge__container active",
-            badgeText: "At Home",
-            icon: <BiHomeAlt className="badge active" />,
-        },
-        {
-            classname: "badge__container active",
-            badgeText: "Adepte des apéros",
-            icon: <BiBeer className="badge active active" />,
-        },
-        {
-            classname: "badge__container active",
-            badgeText: "Souriez, photo",
-            icon: <HiOutlinePhotograph className="badge active" />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "En groupe",
-            icon: <BsPeople className="badge" />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "Créateur de tables",
-            icon: <TbBrandAirtable className="badge" />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "Dernière minute",
-            icon: <IoCalendarOutline className="badge" />,
-        },
-
-        {
-            classname: "badge__container",
-            badgeText: "Accro aux bars",
-            icon: <FaGlassMartiniAlt className="badge" />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "Adepte des restos",
-            icon: <GiMeal className="badge " />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "Fan de l'aléatoire",
-            icon: <GiCartwheel className="badge" />,
-        },
-        {
-            classname: "badge__container",
-            badgeText: "Membre premium",
-            icon: <AiOutlineStar className="badge" />,
-        },
-    ];
 
     return (
         <Layout>
